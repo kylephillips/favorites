9fc0078f63aa0ceb78e2adba88849e2b214d929c
Kyle Phillips <kylephillipsdesign@gmail.com>
2015-03-01 19:37:04 -0500

    functions API comments added.

--- app/API/functions.php
+++ app/API/functions.php
@@ -5,6 +5,9 @@
 
 use SimpleFavorites\Entities\Favorite\FavoriteButton;
 
+/**
+* Get the favorite button
+*/
 function get_simple_favorites_button($post_id = null)
 {
 	if ( !$post_id ) $post_id = get_the_id();
@@ -12,6 +15,9 @@ function get_simple_favorites_button($post_id = null)
 	return $button->display();
 }
 
+/**
+* Echos the favorite button
+*/
 function simple_favorites_button($post_id = null)
 {	
 	echo get_simple_favorites_button($post_id);
