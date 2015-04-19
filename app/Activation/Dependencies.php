5ee85105c7edc3c6145477d87fc26d38a33a9291
Kyle Phillips <kylephillipsdesign@gmail.com>
2015-04-19 17:12:46 -0400

    JS globals added for loading indication

--- app/Activation/Dependencies.php
+++ app/Activation/Dependencies.php
@@ -103,7 +103,11 @@ class Dependencies {
 				'ajaxurl' => admin_url( 'admin-ajax.php' ),
 				'favorite' => $this->settings_repo->buttonText(),
 				'favorited' => $this->settings_repo->buttonTextFavorited(),
-				'includecount' => $this->settings_repo->includeCountInButton()
+				'includecount' => $this->settings_repo->includeCountInButton(),
+				'indicate_loading' => $this->settings_repo->includeLoadingIndicator(),
+				'loading_text' => $this->settings_repo->loadingText(),
+				'loading_image' => $this->settings_repo->loadingImage(),
+				'loading_image_active' => $this->settings_repo->loadingImage('active')
 			)
 		);
 	}
