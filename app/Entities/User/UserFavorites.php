595fd442a566e6845b75869d60bacfb8cf02302e
Kyle Phillips <kylephillipsdesign@gmail.com>
2015-07-12 16:26:37 -0400

    Check for empty array added in user favorites list. Closes #22.

--- app/Entities/User/UserFavorites.php
+++ app/Entities/User/UserFavorites.php
@@ -86,21 +86,19 @@ class UserFavorites {
 		if ( is_null($this->site_id) || $this->site_id == '' ) $this->site_id = get_current_blog_id();
 		$favorites = $this->getFavoritesArray();
 
-		if ( $favorites ){
-			if ( is_multisite() ) switch_to_blog($this->site_id);
-			$out = '<ul class="favorites-list" data-userid="' . $this->user_id . '" data-links="true" data-siteid="' . $this->site_id . '">';
-			foreach ( $favorites as $key => $favorite ){
-				$out .= '<li>';
-				if ( $this->links ) $out .= '<a href="' . get_permalink($favorite) . '">';
-				$out .= get_the_title($favorite);
-				if ( $this->links ) $out .= '</a>';
-				$out .= '</li>';
-			}
-			$out .= '</ul>';
-			if ( is_multisite() ) restore_current_blog();
-			return $out;
+		if ( empty($favorites) ) return false;
+		if ( is_multisite() ) switch_to_blog($this->site_id);
+		$out = '<ul class="favorites-list" data-userid="' . $this->user_id . '" data-links="true" data-siteid="' . $this->site_id . '">';
+		foreach ( $favorites as $key => $favorite ){
+			$out .= '<li>';
+			if ( $this->links ) $out .= '<a href="' . get_permalink($favorite) . '">';
+			$out .= get_the_title($favorite);
+			if ( $this->links ) $out .= '</a>';
+			$out .= '</li>';
 		}
-		return false;
+		$out .= '</ul>';
+		if ( is_multisite() ) restore_current_blog();
+		return $out;
 	}
 
 	/**
