a8945ae01be1fc2cad8bbdfb11f7566caee2dac5
webdev <edwin@yard.nl>
2016-06-03 11:46:30 +0200

    adjusted time interval to 1 year in seconds

--- app/Entities/Favorite/SyncAllFavorites.php
+++ app/Entities/Favorite/SyncAllFavorites.php
@@ -49,7 +49,7 @@ class SyncAllFavorites
 	*/
 	public function cookie()
 	{
-		setcookie( 'simplefavorites', json_encode( $this->favorites ), time() + apply_filters( 'simplefavorites_cookie_expiration_interval' , 3600 ), '/' );
+		setcookie( 'simplefavorites', json_encode( $this->favorites ), time() + apply_filters( 'simplefavorites_cookie_expiration_interval', 31556926 ), '/' );
 		return;
 	}
 
