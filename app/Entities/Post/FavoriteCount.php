e3b9e5dced6c6bdf96b9e4d6218973e09edf9f97
Kyle Phillips <kyle@object9.com>
2017-06-09 15:20:42 -0400

    restore blog added to get all count method

--- app/Entities/Post/FavoriteCount.php
+++ app/Entities/Post/FavoriteCount.php
@@ -27,7 +27,9 @@ class FavoriteCount
 		if ( (is_multisite()) && (isset($site_id)) && ($site_id !== "") ) switch_to_blog(intval($site_id));
 		global $wpdb;
 		$query = "SELECT SUM(meta_value) AS favorites_count FROM {$wpdb->prefix}postmeta WHERE meta_key = 'simplefavorites_count'";
-		return $wpdb->get_var( $query );
+		$count = $wpdb->get_var( $query );
+		if ( (is_multisite()) && (isset($site_id) && ($site_id !== "")) ) restore_current_blog();
+		return intval($count);
 	}
 
 }
