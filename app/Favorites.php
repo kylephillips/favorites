451848c7384d5e393c30a219d9c05b281b684ccf
Kyle Phillips <kyle@object9.com>
2017-06-20 12:58:53 -0400

    Updates readme and version number

--- app/Favorites.php
+++ app/Favorites.php
@@ -12,7 +12,7 @@ class Favorites
 		$favorites_env = 'live';
 
 		global $favorites_version;
-		$favorites_version = '2.1.1';
+		$favorites_version = '2.1.2';
 
 		global $favorites_name;
 		$favorites_name = __('Favorites', 'favorites');
