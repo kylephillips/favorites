c067229fbb043c9b6e05b8b6612983eeeb3ec72a
Kyle Phillips <kyle@object9.com>
2019-01-10 17:31:41 -0500

    updates license copyright year

--- favorites.php
+++ favorites.php
@@ -12,7 +12,7 @@ License: GPLv2 or later.
 Copyright: Kyle Phillips
 */
 
-/*  Copyright 2018 Kyle Phillips
+/*  Copyright 2019 Kyle Phillips
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
