5ba5417cc214ce21a3dd65f8b8d4d13ce1b8e11d
Kyle Phillips <kylephillipsdesign@gmail.com>
2025-04-10 13:25:04 -0400

    Updates the plugin version

--- app/Favorites.php
+++ app/Favorites.php
@@ -12,7 +12,7 @@ class Favorites
 		$favorites_env = 'live';
 
 		global $favorites_version;
-		$favorites_version = '2.3.5';
+		$favorites_version = '2.3.6';
 
 		global $favorites_name;
 		$favorites_name = __('Favorites', 'favorites');
